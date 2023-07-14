27180de8e7dde5ab80d87f74bd6655f4321ad197
shift-48 <shiftenter48@gmail.com>
2023-07-14 08:46:08 +0000

    day and night fixed 80%

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,14 @@
 const input = document.querySelector("input");
 const form = document.querySelector("form");
+const container = document.querySelector(".container");
 let inputText = input.value;
 let total = 0;
+//for the todo item array
+let words = [];
 // document.querySelector('#night').style.display = 'none'
 
+
+container.classList.add('night')
 form.addEventListener("submit", (event) => {
 	event.preventDefault();
 	createDiv();
@@ -26,43 +31,48 @@ function createDiv() {
 	todoItem.classList.add("boxClass");
 	todoItem.innerText = input.value;
 
+	// let count = document.querySelector("#count");
+	// todoItem.classList.toggle("strikethrough");
+	// let hasStrike = todoItem.classList.contains("strikethrough");
+	// if (hasStrike) {
+	// 	total += 1;
+	// 	count.innerText = total;
+	// } else if (!hasStrike) {
+	// 	total -= 5;
+	// 	todoItem.classList.remove("strikethrough");
+	// }
+
+	words.push(todoItem.innerText);
+	console.log(words);
 	todoItem.addEventListener("click", () => {
-		let count = document.querySelector("#count");
-		todoItem.classList.toggle("strikethrough");
+		todoItem.classList.add("strikethrough");
 		let hasStrike = todoItem.classList.contains("strikethrough");
 		if (hasStrike) {
-			total += 1;
-			count.innerText = total;
-		} else if (!hasStrike) {
-			total -= 5;
-			todoItem.classList.remove("strikethrough");
 		}
 	});
 }
 
 function daynNight() {
-	let container = document.querySelector(".container");
-
+	// let container = document.querySelector(".container");
 	let dayBtn = document.querySelector("#day");
 	dayBtn.addEventListener("click", () => {
+		container.style.color = "black";
 		container.classList.add("day");
 		container.classList.remove("night");
-		console.log("hii");
+		// console.log("hii");
 		dayBtn.style.display = "none";
 		nightbtn.style.display = "block";
-		
 	});
 	let nightbtn = document.querySelector("#night");
-	console.log(nightbtn);
+	// console.log(nightbtn);
 	nightbtn.addEventListener("click", () => {
+		container.style.color = "white";
 		container.classList.add("night");
 		container.classList.remove("day");
 		dayBtn.style.display = "block";
 		nightbtn.style.display = "none";
 	});
 }
-
-
 // function night(params) {
 // 	let nightbtn = document.querySelector("#night");
 // 	nightbtn.addEventListener("click", () => {
